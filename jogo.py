cadf6e913b962c8539923437de6b3d528aa286cd
Catmoll1013 <168452129+Catmoll1013@users.noreply.github.com>
2024-05-24 10:04:30 -0300

    Update jogo.py

--- jogo.py
+++ jogo.py
@@ -1,37 +1,37 @@
 import streamlit as st
 
-# Título do jogo
-st.title("Meu Jogo de RPG")
+# Título
+st.title("Jogo Maneiro da Cat")
 
 # Introdução
-st.write("Bem-vindo ao mundo do RPG! Você é o herói desta história.")
+st.write("Bem-vindo ao meu jogo de RPG! Você é o cara desta história.")
 
 # Escolha do personagem
 personagem = st.radio("Escolha seu personagem:",
-                      ("Guerreiro", "Mago", "Arqueiro"))
+                      ("JoJo", "Matheus", "Gecaberalda"))
 
-# Introdução ao personagem escolhido
-if personagem == "Guerreiro":
-    st.write("Você escolheu ser um guerreiro corajoso!")
-elif personagem == "Mago":
-    st.write("Você escolheu ser um poderoso mago!")
+# Descrição dos crias
+if personagem == "JoJo":
+    st.write("Você escolheu ser o doutor programador!")
+elif personagem == "Matheus":
+    st.write("Você escolheu ser o enfermeiro dos dados!")
 else:
-    st.write("Você escolheu ser um ágil arqueiro!")
+    st.write("Você escolheu ser a mãe da fgv!")
 
-# Começo da aventura
-st.write("Você acorda em uma floresta misteriosa...")
+# Pt1
+st.write("Você acorda na sala de programção vazia...")
 
-# Escolha do caminho
-caminho = st.selectbox("Qual caminho você seguirá?",
-                       ("Floresta Profunda", "Montanhas Geladas", "Caverna Escura"))
+# Escolha do caminho pt1
+caminho = st.selectbox("O que você fará a seguir?",
+                       ("7 andar", "Sapinho", "Permanecer"))
 
 # Descrição do caminho escolhido
-if caminho == "Floresta Profunda":
-    st.write("Você decide entrar na densa Floresta Profunda.")
-elif caminho == "Montanhas Geladas":
-    st.write("Você decide escalar as perigosas Montanhas Geladas.")
+if caminho == "7 andar":
+    st.write("Você decide seguir para o 7 andar socializar.")
+elif caminho == "Sapinho":
+    st.write("Você decide seguir para o sapinho almoçar.")
 else:
-    st.write("Você decide explorar a assustadora Caverna Escura.")
+    st.write("Você decide permanecer na sala e programar.")
 
 # Final do jogo
 st.write("E assim começa sua jornada épica...")
